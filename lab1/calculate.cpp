9716ca7c44734f81ff4f4c80236cc3c048e2416a
BannedPooh <rongfei@ucsb.edu>
2021-06-26 01:41:17 +0800

    lab1

---
+++ lab1/calculate.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Please enter 3 numbers."<<std::endl;
+    int a,b,c;
+    std::cin>>a>>b>>c;
+    std::cout<<"The formula result is: "<<3*(a+2*b)-4*c<<std::endl;
+    return 0;
+}
